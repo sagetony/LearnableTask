e225e17cb7cc57251278b77dac9a3b0687857f10
sagetony <uchetony224@gmail.com>
2023-01-15 23:19:43 +0100

    Validate Card

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// NOTE: Card Type: VISA|MASTERCARD|EXPRESS
+
+// test mastercard, visa and express cards;
 const mastercardValidate =
   /^5[1-5]{1}[0-9]{2}[\s]?[0-9]{4}[\s]?[0-9]{4}[\s]?[0-9]{4}$/;
 
@@ -6,7 +9,7 @@ const visaValidate = /^4{1}[0-9]{3}[\s]?[0-9]{4}[\s]?[0-9]{4}[\s]?[0-9]{4}$/;
 const expressValidate =
   /^3[4|7]{1}[0-9]{2}[\s]?[0-9]{4}[\s]?[0-9]{4}[\s]?[0-9]{3}$/;
 
-// NOTE: Card Type: VISA|MASTERCARD|EXPRESS
+// validate the user card details
 const ValidateCard = (cardType, cardNumber) => {
   if (cardType == "MASTERCARD") {
     const cardNumberValidate = mastercardValidate.test(cardNumber);
@@ -34,5 +37,9 @@ const ValidateCard = (cardType, cardNumber) => {
   }
 };
 
+ValidateCard("EXPRESS", 371309266424304);
 
-ValidateCard("VISA", 4056310682833213);
+// List of Cards
+// VISA: 4056310682833213
+// MASTERCARD: 5339182487655281
+// EXPRESS:371309266424304
